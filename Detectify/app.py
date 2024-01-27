4e879a4f22121cc51c87760a5e5819c617cca539
Vaishh9vi <138319608+Vaishh9vi@users.noreply.github.com>
2024-01-27 19:37:07 +0530

    app.py

--- Detectify/app.py
+++ Detectify/app.py
@@ -22,4 +22,53 @@ def predict_deception(sentence):
     sentence_vectorized = vectorizer.transform([sentence])
     
     # Make a prediction using the loaded Naive Bayes model
-    predi
+    prediction = naive_bayes_model.predict(sentence_vectorized)
+    
+    # Return the prediction result and the original sentence
+    return prediction[0], sentence
+
+# Define the route for the home page
+@app.route('/')
+def index():
+    # Render the HTML template for the home page
+    return render_template('index.html')
+
+# Define the route for the FAQ page
+@app.route('/FAQIndex.html')
+def faqs():
+    # Render the HTML template for the FAQ page
+    return render_template('FAQIndex.html')
+
+# Define the route for making predictions based on user input
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Check if the request method is POST
+    if request.method == 'POST':
+        # Get the user input paragraph from the form
+        paragraph = request.form['paragraph']
+        
+        # Split the paragraph into individual sentences
+        sentences = sent_tokenize(paragraph)
+        
+        # Make predictions for each sentence using the predict_deception function
+        predictions = [predict_deception(sentence) for sentence in sentences]
+
+        # Initialize an empty string to store the highlighted paragraph
+        highlighted_paragraph = ""
+
+        # Loop through the predictions and sentences to generate the highlighted paragraph
+        for prediction, sentence in predictions:
+            if prediction == 1:
+                # If the prediction indicates deception, add a highlighted span to the sentence
+                highlighted_paragraph += f'<span style="background-color:  #3fdbdb;">{sentence}</span> '
+            else:
+                # If no deception is predicted, add the original sentence
+                highlighted_paragraph += f'{sentence} '
+
+        # Render the home page template with the highlighted paragraph and predictions
+        return render_template('index.html', paragraph=highlighted_paragraph, predictions=predictions)
+
+# Run the Flask app if the script is executed directly
+if __name__ == '__main__':
+    app.run(debug=True)
+
