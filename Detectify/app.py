9c97215652008e5b6f036ac79427c03a25f21bb8
Vaishh9vi <138319608+Vaishh9vi@users.noreply.github.com>
2024-01-27 19:36:15 +0530

    Update app.py

--- Detectify/app.py
+++ Detectify/app.py
@@ -1,52 +1,25 @@
 # app.py
 
+# Import necessary libraries and modules
 from flask import Flask, render_template, request
 import joblib
 import nltk
 from nltk.tokenize import sent_tokenize
 
+# Download the punkt module from NLTK for sentence tokenization
 nltk.download('punkt')
 
+# Initialize the Flask app and define the static URL path
 app = Flask(__name__, static_url_path='/static')
 
-# Load the trained model and vectorizer
+# Load the trained Naive Bayes model and vectorizer from joblib files
 naive_bayes_model = joblib.load('naive_bayes_model.joblib')
 vectorizer = joblib.load('vectorizer.joblib')
 
+# Function to predict deception for a given sentence
 def predict_deception(sentence):
-    # Convert input text to numerical data using the loaded vectorizer
+    # Convert the input text to numerical data using the loaded vectorizer
     sentence_vectorized = vectorizer.transform([sentence])
-    # Make a prediction using the loaded model
-    prediction = naive_bayes_model.predict(sentence_vectorized)
-    return prediction[0], sentence
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/FAQIndex.html')
-def faqs():
-    return render_template('FAQIndex.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        paragraph = request.form['paragraph']
-        # Split the paragraph into sentences
-        sentences = sent_tokenize(paragraph)
-        
-        # Make predictions for each sentence
-        predictions = [predict_deception(sentence) for sentence in sentences]
-
-        highlighted_paragraph = ""
-        for prediction, sentence in predictions:
-            if prediction == 1:
-                highlighted_paragraph += f'<span style="background-color:  #3fdbdb;">{sentence}</span> '
-            else:
-                highlighted_paragraph += f'{sentence} '
-
-        return render_template('index.html', paragraph=highlighted_paragraph, predictions=predictions)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    
+    # Make a prediction using the loaded Naive Bayes model
+    predi
